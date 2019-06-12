f4da42172542e55500b534e2f656ec499b851dd5
Vikram S <vikram.san@gmail.com>
2019-06-12 09:33:22 -0400

    Adding DB credentials & Tier info to unclassify script

--- software/SIW/unclassify.sh
+++ software/SIW/unclassify.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-export UNCLASSIFY_ARGS=`echo "$@"`
-if [[ -z $1 || -z $2 ]]; then
+if [[ -z $1 || -z $2 || -z $3 || -z $4 || -z $5 ]]; then
 	echo "Script usage: "
-	echo "./unclassify.sh <CSName> <CSVersion>"
+	echo "./unclassify.sh <CSName> <CSVersion> <DB URL> <DB USER> <DB PWD>"
 	exit 1;
 else 	 
-	mvn exec:java@unclassify -Dexec.args="$UNCLASSIFY_ARGS"
+	mvn exec:java@unclassify -Dexec.args="$1 $2" -Ddb.url=$3 -Ddb.user=$4 -Ddb.passwd=$5
 fi
